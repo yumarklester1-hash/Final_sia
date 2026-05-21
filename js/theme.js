cdb298bafaa50478aeef3ee2e453109258094308
Code_069 <138640824+Mendukusaiiiii@users.noreply.github.com>
2026-05-21 18:05:03 +0800

    Add files via upload

--- js/theme.js
+++ js/theme.js
@@ -6,6 +6,6 @@ function initTheme() {
   btn.addEventListener("click", () => {
     document.body.classList.toggle("light");
     const isLight = document.body.classList.contains("light");
-    btn.innerHTML = isLight ? '<img src="images/light.svg" class="icon"> Dark Mode' : '<img src="images/dark.svg" class="icon"> Light Mode';
+    btn.innerHTML = isLight ? '<img src="images/dark.svg" class="icon"> Dark Mode' : '<img src="images/light.svg" class="icon"> Light Mode';
   });
 }
